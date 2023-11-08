b5bef03e0b222da0b0b31695c68f3d1c9a9d1d1e
DanielQuinn53 <150138361+DanielQuinn53@users.noreply.github.com>
2023-11-08 14:58:35 +0000

    Adding a Home Page and Added TResting alert

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Sanitary Product Management System")
+md = 'Welcome to the Sanitary Product Management System designed by Robert Craig, Daniel Quinn and Adam Exley'
+st.markdown(md)
+st.toast('FUCKED ⚠️')
