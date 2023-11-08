8b58900a1848abde8a9cee303c1dce1d6bf5edd9
DanielQuinn53 <150138361+DanielQuinn53@users.noreply.github.com>
2023-11-08 15:02:15 +0000

    Rename About.py to Pages/About.py

--- About.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title("About: Sanitary Product Management System")
---
+++ Pages/About.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import pandas as pd
+
+st.title("About: Sanitary Product Management System")
