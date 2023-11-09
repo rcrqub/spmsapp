c6ff3e3108521452af941429f664ee6974af1cc9
40299952 <dquinn53@qub.ac.uk>
2023-11-09 12:04:27 +0000

    Added a simple Implementation of the Low Stock error

--- Home.py
+++ Home.py
@@ -1,7 +1,19 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pd
 
+# Home Page Text and Setup
+st.set_page_config(page_title="SPMS",page_icon="🚽",)
 st.title("Sanitary Product Management System")
-md = 'Welcome to the Sanitary Product Management System designed by Robert Craig, Daniel Quinn and Adam Exley'
+md = 'Welcome :wave: to the Sanitary Product Management System designed by *Robert Craig*, *Daniel Quinn* and *Adam Exley* for the CSC4008: Digital Transformation module.'
 st.markdown(md)
-st.toast('FUCKED ⚠️')
+
+# Importing Bathroom Data
+df = pd.read_csv('BathroomData.csv')
+st.write(df)
+
+st.markdown('**Stock Low**')
+# Stock Warnings (Toast Notification)
+stock_warning_thres = 3
+for _, row in df.iterrows():
+    if row['stock_level'] <= stock_warning_thres:
+        st.error(f"{row['item_type']} is low in Bathroom {row['bathroom_id']}", icon="❗")
