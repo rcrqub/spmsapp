de8cd2802992efb37f8a6546635c788fcbef1e3b
40299952 <dquinn53@qub.ac.uk>
2023-11-14 10:59:32 +0000

    Added Redistribution
    Added an easy average count to suggest which products to move where it doesn't work with non-integers

--- Home.py
+++ Home.py
@@ -1,8 +1,9 @@
+from typing import Type
 import streamlit as st 
 import pandas as pd
 
 # Home Page Text and Setup
-st.set_page_config(page_title="SPMS",page_icon="🚽",)
+st.set_page_config(page_title="SPMS",page_icon=':toilet:')
 st.title("Sanitary Product Management System")
 md = 'Welcome :wave: to the Sanitary Product Management System designed by *Robert Craig*, *Daniel Quinn* and *Adam Exley* for the CSC4008: Digital Transformation module.'
 st.markdown(md)
@@ -11,9 +12,8 @@ st.markdown(md)
 df = pd.read_csv('BathroomData.csv')
 st.write(df)
 
-st.markdown('**Stock Low**')
-# Stock Warnings (Toast Notification)
-stock_warning_thres = 3
+st.subheader('Stock Low')
+stock_warning_thres = st.number_input("Please set the Stock Warning Threshold: ", step = 1, placeholder="Type a number...")
 for _, row in df.iterrows():
     if row['stock_level'] <= stock_warning_thres:
-        st.error(f"{row['item_type']} is low in Bathroom {row['bathroom_id']}", icon="❗")
+        st.write(f" ❗ {row['item_type']} is low in Bathroom {row['bathroom_id']}")
