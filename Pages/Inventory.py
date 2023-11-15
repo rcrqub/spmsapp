198f41729e528053a7e7dc752bc3c5652f4f9f2c
40299952 <dquinn53@qub.ac.uk>
2023-11-15 20:18:41 +0000

    Some clean up on the UI

--- Pages/Inventory.py
+++ Pages/Inventory.py
@@ -8,15 +8,18 @@ df = pd.read_csv('BathroomData.csv')
 
 selection = st.selectbox(
     'Which Product would you like to Redistrubute?', 
-    ("heavy_pad","medium_pad","light_pad"),
+    ("Heavy Pad","Medium Pad","Light Pad"),
     placeholder = "Select Product")
 
 st.write('You selected:', selection) 
+
+selection = selection.replace(" ", "_")
+selection = selection.lower()
 
 selected_df = df.loc[df['item_type'] == selection]
 mean = selected_df.mean(axis=0, numeric_only = True)
 for _, row in selected_df.iterrows():
     if int(mean['stock_level']) - row['stock_level'] < 0:
-        st.write(f" Please Remove {row['stock_level']-int(mean['stock_level'])} Items from Bathroom {row['bathroom_id']}")
+        st.write(f" :: Please Remove {row['stock_level']-int(mean['stock_level'])} Items from Bathroom {row['bathroom_id']}")
     else:
         st.write(f" Please Add {int(mean['stock_level']) - row['stock_level']} Items to Bathroom {row['bathroom_id']}")
--- Pages/report.py
+++ Pages/report.py
@@ -2,4 +2,4 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="SPMS",page_icon=':toilet:')
-st.title("Reporting: Sanitary Product Management System")
+st.title("Reporting")
