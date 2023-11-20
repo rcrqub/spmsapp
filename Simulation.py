f4deb9ee02097ade37d36ba009c980aa3ba51e82
aequb <150683257+aequb@users.noreply.github.com>
2023-11-20 18:15:16 +0000

    Update Simulation.py
    Prints empty stock alerts

--- Simulation.py
+++ Simulation.py
@@ -21,8 +21,13 @@ def stockUsed():
     return random.choices(options, weights=weights)[0]
 
 start_date = datetime(2024, 1, 1, 8, 0) # Start date = 1st Jan 2024
-end_date = datetime(2024, 1, 1, 22, 0) # End date = 1st Jan 2025
+end_date = datetime(2024, 1, 1, 22, 0) # End date = 1st Jan 2024
 delta = timedelta(hours=1)  # Iterate by 1 hour
+
+zeroAlerts = [] #Keeps track of whether the "stock empty" alert has come up yet
+for itemID in df['stock_level']:
+    zeroAlerts.append(False)
+
 print(df)
 while start_date <= end_date:
     action = randomise()
@@ -30,4 +35,10 @@ while start_date <= end_date:
     #time.sleep(.5)
     if start_date.hour == 22: start_date += 10*delta # If time is 10pm move to 8am
     else: start_date += delta # Add 1 hour to the time
+    
+    #alert if [bathroom/product] empty
+    for index in range(0,len(df['stock_level'])-1):
+        if (df['stock_level'][index] == 0) and (not zeroAlerts[index]):
+            zeroAlerts[index] = True #sets alert flag
+            print("ALERT: Empty stock! Bathroom " + str(df['bathroom_id'][index]) + ", item " + str(df['item_type'][index]))
 print(df)
