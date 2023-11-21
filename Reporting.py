a8587fe926b960becdbd4f594a9e4823195cdf8d
rrobberrtt <robertandrewcraig@icloud.com>
2023-11-21 11:45:40 +0000

    Simulation and Reporting Update

---
+++ Reporting.py
@@ -0,0 +1,17 @@
+from Simulation import Simulation
+from csv import DictWriter
+
+fieldNames = ['year','month','day','hour','bathroom_id','item_type','stock_level']
+
+sim = Simulation()
+dayData = sim.simulateDay()
+
+
+with open('test.csv', 'w') as file:
+    write = DictWriter(file, fieldnames=fieldNames)
+    write.writeheader()
+    for day in dayData:
+        for hour in day:
+            dict1 = dict(zip(fieldNames, hour))
+            print(dict1)
+            write.writerow(dict1)
