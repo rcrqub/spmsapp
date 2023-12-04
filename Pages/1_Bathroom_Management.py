4d24fe30b3e30727a4afbb0be17e11f64c29b32c
40299952 <dquinn53@qub.ac.uk>
2023-12-04 19:24:50 +0000

    Added user auth. to all pages

--- Pages/1_Bathroom_Management.py
+++ Pages/1_Bathroom_Management.py
@@ -1,72 +1,121 @@
-import streamlit as st
-import pandas as pd
 from streamlit_float import float_dialog
+from typing import Type
+import streamlit as st 
+import pandas as pd
+import altair as alt
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
 
-st.set_page_config(page_title="SPMS", page_icon=':toilet:')
-st.title("Bathroom Management")
+# User Auth
+names = ['Daniel Quinn', 'Adam Exley', 'Robert Craig']
+usernames = ['dquinn','aexley','rcraig']
 
-df = pd.read_csv('ReportingData.csv')
-pd.to_datetime(df[['year','month','day','hour']])
-current_date = df[['year','month','day','hour']].max()
-current_data = df.loc[(df['year'] == current_date['year'])
-                     & (df['month'] == current_date['month'])
-                     & (df['day'] == current_date['day'])
-                     & (df['hour'] == current_date['hour'])]
+file_path = Path(__file__).parent.parent / "hased_pw.pk1"
+with file_path.open('rb') as file:
+    hashed_passwords = pickle.load(file)
 
-md = 'Here you can create new bathrooms or remove old ones. Additionally, you can manually manage the products you have available.'
-st.markdown(md)
+credentials = {
+    "usernames":{
+        usernames[0]:{
+            "name":names[0],
+            "password":hashed_passwords[0]
+            },
+        usernames[1]:{
+            "name":names[1],
+            "password":hashed_passwords[1]
+            }, 
+        usernames[2]:{
+            "name":names[2],
+            "password":hashed_passwords[2]
+            }         
+        }
+    }
 
-IDS = current_data['bathroom_id'].unique()
-DESC = current_data['bathroom_description'].unique()
+authenticator = stauth.Authenticate(credentials, "spms", "auth", cookie_expiry_days=30)
 
-def add_bathroom(text):
-    new_bathroom = {'bathroom_id': len(IDS) + 1, 'bathroom_description': text}
-    current_data.loc[len(current_data)] = new_bathroom
-    current_data.to_csv('BathroomData.csv', index=False)
+# Create a session state object
+if "authentication_status" not in st.session_state:
+    st.session_state.authentication_status = None
 
-def remove_bathroom(bathroom_id):
-    current_data.drop(current_data[current_data.bathroom_id == bathroom_id].index, inplace=True)
-    current_data.to_csv('BathroomData.csv', index=False)
+name, authentication_status, username = authenticator.login("Login to the SPMS", 'main')
 
-st.subheader('Available Bathrooms :toilet:')
+# Handle authentication status
+if authentication_status is False:
+    st.error('Username/password is incorrect')
+elif authentication_status is None:
+    st.warning('Please enter your username and password')
+elif authentication_status:
 
-if "show" not in st.session_state:
-    st.session_state.show = False
+    # Set the authentication status in session state
+    st.session_state.authentication_status = authentication_status
 
-# Create Float Dialog container
-dialog_container = float_dialog(st.session_state.show)
+    # Introduction
+    authenticator.logout("Logout", 'sidebar')
+    st.header("Bathroom and Product Management")
 
-# Add contents of Dialog including button to close it
-with dialog_container:
-    add_input = st.text_input("Enter a Bathroom Description", key='add')
-    if st.button("Accept", key="send"):
-        if add_input.strip() != "":
-            add_bathroom(add_input)
-            st.session_state.show = False
-            st.experimental_rerun()
+    df = pd.read_csv('ReportingData.csv')
+    pd.to_datetime(df[['year','month','day','hour']])
+    current_date = df[['year','month','day','hour']].max()
+    current_data = df.loc[(df['year'] == current_date['year'])
+                        & (df['month'] == current_date['month'])
+                        & (df['day'] == current_date['day'])
+                        & (df['hour'] == current_date['hour'])]
 
-# Refresh IDS and DESC after adding a new bathroom
-IDS = current_data['bathroom_id'].unique()
-DESC = current_data['bathroom_description'].unique()
+    md = 'Here you can create new bathrooms or remove old ones. Additionally, you can manually manage the products you have available.'
+    st.markdown(md)
 
-col1, col2, col3 = st.columns(3)
+    IDS = current_data['bathroom_id'].unique()
+    DESC = current_data['bathroom_description'].unique()
 
-with col1:
-    st.markdown("*Bathroom ID*")
-    for bathroom_id in IDS:
-        st.write(f"{bathroom_id}")
-    if st.button('🟢 Add Bathroom'):
-        st.session_state.show = True
+    def add_bathroom(text):
+        new_bathroom = {'bathroom_id': len(IDS) + 1, 'bathroom_description': text}
+        current_data.loc[len(current_data)] = new_bathroom
+        current_data.to_csv('BathroomData.csv', index=False)
 
-with col2:
-    st.markdown("*Description*")
-    for description in DESC:
-        st.write(f"{description}")
+    def remove_bathroom(bathroom_id):
+        current_data.drop(current_data[current_data.bathroom_id == bathroom_id].index, inplace=True)
+        current_data.to_csv('BathroomData.csv', index=False)
 
-with col3:
-    st.markdown("*Remove Bathroom*")
-    for bathroom_id in IDS:
-        if st.button(f'❌ Remove', key=bathroom_id):
-            remove_bathroom(bathroom_id)
-            st.experimental_rerun()
+    st.subheader('Available Bathrooms :toilet:')
+
+    if "show" not in st.session_state:
+        st.session_state.show = False
+
+    # Create Float Dialog container
+    dialog_container = float_dialog(st.session_state.show)
+
+    # Add contents of Dialog including button to close it
+    with dialog_container:
+        add_input = st.text_input("Enter a Bathroom Description", key='add')
+        if st.button("Accept", key="send"):
+            if add_input.strip() != "":
+                add_bathroom(add_input)
+                st.session_state.show = False
+                st.experimental_rerun()
+
+    # Refresh IDS and DESC after adding a new bathroom
+    IDS = current_data['bathroom_id'].unique()
+    DESC = current_data['bathroom_description'].unique()
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.markdown("*Bathroom ID*")
+        for bathroom_id in IDS:
+            st.write(f"{bathroom_id}")
+        if st.button('🟢 Add Bathroom'):
+            st.session_state.show = True
+
+    with col2:
+        st.markdown("*Description*")
+        for description in DESC:
+            st.write(f"{description}")
+
+    with col3:
+        st.markdown("*Remove Bathroom*")
+        for bathroom_id in IDS:
+            if st.button(f'❌ Remove', key=bathroom_id):
+                remove_bathroom(bathroom_id)
+                st.experimental_rerun()
 
--- Pages/2_Ineventory_Redistribution.py
+++ Pages/2_Ineventory_Redistribution.py
@@ -1,93 +1,145 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from typing import Type
+import altair as alt
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
 
-def redistribute_items(df):
+# User Auth
+names = ['Daniel Quinn', 'Adam Exley', 'Robert Craig']
+usernames = ['dquinn','aexley','rcraig']
 
-    # Group by bathroom_id and item_type, and calculate the total stock_level for each group
-    grouped_df = df.groupby(['bathroom_id', 'item_type'])['stock_level'].sum().reset_index()
+file_path = Path(__file__).parent.parent / "hased_pw.pk1"
+with file_path.open('rb') as file:
+    hashed_passwords = pickle.load(file)
 
-    # Calculate the average stock_level for each item_type
-    avg_stock_per_item = grouped_df.groupby('item_type')['stock_level'].mean()
+credentials = {
+    "usernames":{
+        usernames[0]:{
+            "name":names[0],
+            "password":hashed_passwords[0]
+            },
+        usernames[1]:{
+            "name":names[1],
+            "password":hashed_passwords[1]
+            }, 
+        usernames[2]:{
+            "name":names[2],
+            "password":hashed_passwords[2]
+            }         
+        }
+    }
 
-    # Function to redistribute stock evenly across bathrooms for a given item_type
-    def redistribute_item_type(group):
-        item_type = group['item_type'].iloc[0]
-        group['stock_level'] = np.round(avg_stock_per_item[item_type])
-        return group
+authenticator = stauth.Authenticate(credentials, "spms", "auth", cookie_expiry_days=30)
 
-    # Apply the redistribution function to each group (item_type)
-    redistributed_df = grouped_df.groupby('item_type').apply(redistribute_item_type)
+# Create a session state object
+if "authentication_status" not in st.session_state:
+    st.session_state.authentication_status = None
 
-    # Reset index to avoid MultiIndex
-    redistributed_df = redistributed_df.reset_index(drop=True)
+name, authentication_status, username = authenticator.login("Login to the SPMS", 'main')
 
-    # Sort the redistributed data by bathroom_id and item_type
-    redistributed_df = redistributed_df.sort_values(by=['bathroom_id', 'item_type'])
+# Handle authentication status
+if authentication_status is False:
+    st.error('Username/password is incorrect')
+elif authentication_status is None:
+    st.warning('Please enter your username and password')
+elif authentication_status:
 
-    # Convert stock_level column to integers
-    redistributed_df['stock_level'] = redistributed_df['stock_level'].astype(int)
+    # Set the authentication status in session state
+    st.session_state.authentication_status = authentication_status
 
-    # Calculate the difference between original and redistributed stock levels
-    redistributed_df['Difference'] = redistributed_df.apply(
-        lambda row: "{}{}".format(
-            '+' if row['stock_level'] - df.loc[
-                (df['bathroom_id'] == row['bathroom_id']) & (df['item_type'] == row['item_type']),
-                'stock_level'
-            ].iloc[0] > 0 else '',
-            row['stock_level'] - df.loc[
-                (df['bathroom_id'] == row['bathroom_id']) & (df['item_type'] == row['item_type']),
+    # Introduction
+    authenticator.logout("Logout", 'sidebar')
+
+    def redistribute_items(df):
+
+        # Group by bathroom_id and item_type, and calculate the total stock_level for each group
+        grouped_df = df.groupby(['bathroom_id', 'item_type'])['stock_level'].sum().reset_index()
+
+        # Calculate the average stock_level for each item_type
+        avg_stock_per_item = grouped_df.groupby('item_type')['stock_level'].mean()
+
+        # Function to redistribute stock evenly across bathrooms for a given item_type
+        def redistribute_item_type(group):
+            item_type = group['item_type'].iloc[0]
+            group['stock_level'] = np.round(avg_stock_per_item[item_type])
+            return group
+
+        # Apply the redistribution function to each group (item_type)
+        redistributed_df = grouped_df.groupby('item_type').apply(redistribute_item_type)
+
+        # Reset index to avoid MultiIndex
+        redistributed_df = redistributed_df.reset_index(drop=True)
+
+        # Sort the redistributed data by bathroom_id and item_type
+        redistributed_df = redistributed_df.sort_values(by=['bathroom_id', 'item_type'])
+
+        # Convert stock_level column to integers
+        redistributed_df['stock_level'] = redistributed_df['stock_level'].astype(int)
+
+        # Calculate the difference between original and redistributed stock levels
+        redistributed_df['Difference'] = redistributed_df.apply(
+            lambda row: "{}{}".format(
+                '+' if row['stock_level'] - df.loc[
+                    (df['bathroom_id'] == row['bathroom_id']) & (df['item_type'] == row['item_type']),
+                    'stock_level'
+                ].iloc[0] > 0 else '',
+                row['stock_level'] - df.loc[
+                    (df['bathroom_id'] == row['bathroom_id']) & (df['item_type'] == row['item_type']),
+                    'stock_level'
+                ].iloc[0]
+            ),
+            axis=1
+        )
+
+        return redistributed_df
+
+
+    def redistribution_summary(original_df, redistributed_df):
+        summary = []
+        
+        for index, row in original_df.iterrows():
+            bathroom_id = row['bathroom_id']
+            item_type = row['item_type']
+            original_stock = row['stock_level']
+            redistributed_stock = redistributed_df.loc[
+                (redistributed_df['bathroom_id'] == bathroom_id) & (redistributed_df['item_type'] == item_type),
                 'stock_level'
             ].iloc[0]
-        ),
-        axis=1
-    )
 
-    return redistributed_df
+            difference = redistributed_stock - original_stock
+            difference_str = f"&#43;{difference}" if difference > 0 else str(difference) #&#43;='+' in ASCII
 
+            summary.append({
+                'Bathroom ID': bathroom_id,
+                'Item Type': item_type,
+                'Original Stock': original_stock,
+                'Redistributed Stock': redistributed_stock,
+                'Difference': difference_str
+            })
+        
+        summary_df = pd.DataFrame(summary)
 
-def redistribution_summary(original_df, redistributed_df):
-    summary = []
-    
-    for index, row in original_df.iterrows():
-        bathroom_id = row['bathroom_id']
-        item_type = row['item_type']
-        original_stock = row['stock_level']
-        redistributed_stock = redistributed_df.loc[
-            (redistributed_df['bathroom_id'] == bathroom_id) & (redistributed_df['item_type'] == item_type),
-            'stock_level'
-        ].iloc[0]
+        # Sort the summary by item_type and then by bathroom_id
+        summary_df = summary_df.sort_values(by=['Item Type', 'Bathroom ID'])
+        display_markdown_table(summary_df, "Redistribution Summary")
 
-        difference = redistributed_stock - original_stock
-        difference_str = f"&#43;{difference}" if difference > 0 else str(difference) #&#43;='+' in ASCII
+    def display_markdown_table(df, title):
+        st.subheader(title)
+        st.markdown(df.to_markdown(index=False), unsafe_allow_html=True)
 
-        summary.append({
-            'Bathroom ID': bathroom_id,
-            'Item Type': item_type,
-            'Original Stock': original_stock,
-            'Redistributed Stock': redistributed_stock,
-            'Difference': difference_str
-        })
-    
-    summary_df = pd.DataFrame(summary)
+    # Read the CSV data from a file
+    file_path = "ReportingDataMonth.csv"
+    df = pd.read_csv(file_path)
 
-    # Sort the summary by item_type and then by bathroom_id
-    summary_df = summary_df.sort_values(by=['Item Type', 'Bathroom ID'])
-    display_markdown_table(summary_df, "Redistribution Summary")
+    df_filtered = df.groupby(['bathroom_id', 'item_type']).tail(1)
+    bathroomData = df_filtered.drop(["year", "month", "day", "hour"], axis=1)
+    dist = redistribute_items(bathroomData)
 
-def display_markdown_table(df, title):
-    st.subheader(title)
-    st.markdown(df.to_markdown(index=False), unsafe_allow_html=True)
-
-# Read the CSV data from a file
-file_path = "ReportingDataMonth.csv"
-df = pd.read_csv(file_path)
-
-df_filtered = df.groupby(['bathroom_id', 'item_type']).tail(1)
-bathroomData = df_filtered.drop(["year", "month", "day", "hour"], axis=1)
-dist = redistribute_items(bathroomData)
-
-# Page configuration
-st.set_page_config(page_title="SPMS", page_icon="🚽")
-st.title("Inventory Redistribution")
-redistribution_summary(bathroomData, dist)
+    # Page configuration
+    st.header("Inventory Redistribution")
+    md = 'Here you can find the steps needed to evenly distribute the products across all bathrooms.'
+    st.markdown(md)
+    redistribution_summary(bathroomData, dist)
